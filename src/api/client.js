0e1aa1c5a60367a144beb8ff989ae246951c5f82
AndersonBem <126602982+AndersonBem@users.noreply.github.com>
2026-05-27 12:50:49 -0300

    Inicio dir api

---
+++ src/api/client.js
@@ -0,0 +1,36 @@
+const API_BASE_URL = 'https://acad-time.onrender.com'
+
+let accessToken = null;
+
+export function setAccessToken(token) {
+    accessToken = token;
+}
+
+export function getAccessToken(){
+    return accessToken;
+}
+
+export async function apiRequest(path, options = {}) {
+    const isFormData = options.body instanceof FormData;
+
+    const headers = {
+        Accept: "application/json",
+        ...(isFormData ? {} : { 'Content-Type': 'application/json' }),
+        ...(accessToken ? { Authorization: `Bearer ${accessToken}` } : {}),
+        ...options.headers,
+    }
+
+    const response = await fetch(`${API_BASE_URL}${path}`, {
+        ...options,
+        headers,
+    });
+
+    const text = await response.text();
+    const data = text ? JSON.parse(text) : null;
+
+    if (!response.ok) {
+        throw data || { detal: 'Erro na requisição.'};
+    }
+
+    return data
+}
